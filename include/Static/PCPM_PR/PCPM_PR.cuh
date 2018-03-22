24979cbaec443f70315916f0d87861c6026e0830
Alok Tripathy <kullalok@gmail.com>
2018-03-22 13:30:55 -0400

    Removed kcore header and included pr header.

--- include/Static/PCPM_PR/PCPM_PR.cuh
+++ include/Static/PCPM_PR/PCPM_PR.cuh
@@ -8,20 +8,23 @@
 #include <Core/GPUCsr/Csr.cuh>
 #include <Core/GPUHornet/Hornet.cuh>
 
+#include <stdint.h>
+
+#define NUM_PARTS 1
+
 namespace hornets_nest {
 
 using HornetGraph = gpu::Hornet<EMPTY, EMPTY>;
 
-struct KCoreData {
-    vid_t *src;
-    vid_t *dst;
-    int counter;
+struct MsgData {
+    float val;
+    vid_t dst;
 };
 
-class KCore : public StaticAlgorithm<HornetGraph> {
+class PCPM_PR : public StaticAlgorithm<HornetGraph> {
 public:
-    KCore(HornetGraph &hornet);
-    ~KCore();
+    PCPM_PR(HornetGraph& hornet);
+    ~PCPM_PR();
 
     void reset()    override;
     void run()      override;
@@ -32,21 +35,20 @@ private:
     // HostDeviceVar<KCoreData> hd_data;
 
     //load_balancing::BinarySearch load_balancing;
-    load_balancing::VertexBased1 load_balancing;
+
+    vid_t *hornet_csr_off[NUM_PARTS];
+    vid_t *hornet_csr_edges[NUM_PARTS];
 
     TwoLevelQueue<vid_t> vqueue;
     TwoLevelQueue<vid_t> src_equeue;
     TwoLevelQueue<vid_t> dst_equeue;
-    TwoLevelQueue<vid_t> peel_vqueue;
-    // TwoLevelQueue<vid_t> tot_src_equeue;
-    // TwoLevelQueue<vid_t> tot_dst_equeue;
 
-    vid_t *vertex_pres { nullptr };
-    vid_t *h_copy_csr_off { nullptr };
-    vid_t *h_copy_csr_edges { nullptr };
+    float *pr;
 
     // HostDeviceVar<KCoreData> hd_data;
     // MultiLevelQueue<vid_t> nodes_removed;
+
+    load_balancing::VertexBased1 load_balancing;
 };
 
 }
